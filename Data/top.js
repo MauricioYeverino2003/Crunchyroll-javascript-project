147cdb9f79504e30f75ef76aa9cd436a5bdfeec6
Mauricio Yeverino <maukoyi@hotmail.com>
2023-07-20 08:21:12 -0700

    Updated list of elements (anime data)

--- Data/top.js
+++ Data/top.js
@@ -40,6 +40,422 @@ const topPicks = [
     info: 'Subtitled',
     src: 'Images/Posters/the-world-only-god-knows-poster.jpg',
     link: 'https://www.crunchyroll.com/series/GR9PV5MD6/the-world-god-only-knows'
+  },
+
+  {
+    name: 'In Another World With My Smartphone',
+    info: 'Sub | Dub',
+    src: 'Images/Posters/in-another-world-with-my-smartphone-poster.jpg',
+    link: 'https://www.crunchyroll.com/series/GYQ4ZWV46/in-another-world-with-my-smartphone'
+  },
+
+  {
+    name: 'AMAIM Warrior at the Borderline',
+    info: 'Sub | Dub',
+    src: 'Images/Posters/amaim-warrior-at-the-borderline-poster.jpg',
+    link: 'https://www.crunchyroll.com/series/GJ0H7QXE2/amaim-warrior-at-the-borderline'
+  },
+
+  {
+    name: 'MOBILE SUIT GUNDAM UNICORN RE:0096',
+    info: 'Subtitled',
+    src: 'Images/Posters/mobile-suit-gundam-unicorn-poster.jpg',
+    link: 'https://www.crunchyroll.com/series/G6MGP9W76/mobile-suit-gundam-unicorn-re0096'
+  },
+
+  {
+    name: 'takt op.Destiny',
+    info: 'Sub | Dub',
+    src: 'Images/Posters/takt-op.destiny-poster.jpg',
+    link: 'https://www.crunchyroll.com/series/G1XHJVXX2/takt-opdestiny'
+  },
+
+  {
+    name: 'AMAZING STRANGER',
+    info: 'Subtitled',
+    src: 'Images/Posters/amazing-stranger-poster.jpg',
+    link: 'https://www.crunchyroll.com/series/G6J0MD1ER/amazing-stranger'
+  },
+
+  {
+    name: 'Dragon Ball Super',
+    info: 'Subtitled',
+    src: 'Images/Posters/dragon-ball-super-poster.jpg',
+    link: 'https://www.crunchyroll.com/series/GR19V7816/dragon-ball-super'
+  },
+
+  {
+    name: 'JUJUTSU KAISEN',
+    info: 'Sub | Dub',
+    src: 'Images/Posters/jujutsu-kaisen-poster.jpg',
+    link: 'https://www.crunchyroll.com/series/GRDV0019R/jujutsu-kaisen'
+  },
+
+  {
+    name: 'RADIANT',
+    info: 'Sub | Dub',
+    src: 'Images/Posters/radiant-poster.jpg',
+    link: 'https://www.crunchyroll.com/series/G6W4MEZ0R/radiant'
+  },
+
+  {
+    name: 'Mob Psycho 100',
+    info: 'Sub | Dub',
+    src: 'Images/Posters/mob-psycho-poster.jpg',
+    link: 'https://www.crunchyroll.com/series/GY190DKQR/mob-psycho-100'
+  },
+
+  {
+    name: 'TRIGUN STAMPEDE',
+    info: 'Sub | Dub',
+    src: 'Images/Posters/trigun-stampede-poster.jpg',
+    link: 'https://www.crunchyroll.com/series/GXJHM3PK5/trigun-stampede'
+  },
+
+  {
+    name: 'SABIKUI BISKO',
+    info: 'Sub | Dub',
+    src: 'Images/Posters/sabikui-bisco-poster.jpg',
+    link: 'https://www.crunchyroll.com/series/G79H23V24/sabikui-bisco'
+  },
+
+  {
+    name: 'My Hero Academia',
+    info: 'Subtitled',
+    src: 'Images/Posters/my-hero-academia-poster.jpg',
+    link: 'https://www.crunchyroll.com/series/G6NQ5DWZ6/my-hero-academia'
+  },
+
+  {
+    name: 'DRAGON QUEST The Adventure of Dai',
+    info: 'Subtitled',
+    src: 'Images/Posters/dragon-quest-poster.jpg',
+    link: 'https://www.crunchyroll.com/series/GYXM79M56/dragon-quest-the-adventure-of-dai'
+  },
+
+  {
+    name: '86 EIGHTY-SIX',
+    info: 'Sub | Dub',
+    src: 'Images/Posters/86-poster.jpg',
+    link: 'https://www.crunchyroll.com/series/GVDHX8DM5/86-eighty-six'
+  },
+
+  {
+    name: 'Cells at Work!',
+    info: 'Sub | Dub',
+    src: 'Images/Posters/cells-at-work-poster.jpg',
+    link: 'https://www.crunchyroll.com/series/GR3KVPQER/cells-at-work'
+  },
+
+  {
+    name: 'Fruits Basket',
+    info: 'Sub | Dub',
+    src: 'Images/Posters/fruits-basket-poster.jpg',
+    link: 'https://www.crunchyroll.com/series/G6ZJMGEXY/fruits-basket'
+  },
+
+  {
+    name: 'Naruto',
+    info: 'Subtitled',
+    src: 'Images/Posters/naruto-poster.jpg',
+    link: 'https://www.crunchyroll.com/series/GY9PJ5KWR/naruto'
+  },
+
+  {
+    name: 'Naruto Shippuden',
+    info: 'Subtitled',
+    src: 'Images/Posters/naruto-shippuden-poster.jpg',
+    link: 'https://www.crunchyroll.com/series/GYQ4MW246/naruto-shippuden'
+  },
+
+  {
+    name: 'Tokyo Ghoul',
+    info: 'Sub | Dub',
+    src: 'Images/Posters/tokyo-ghoul-poster.jpg',
+    link: 'https://www.crunchyroll.com/series/G6NV7Z50Y/tokyo-ghoul'
+  },
+
+  {
+    name: 'BLUELOCK',
+    info: 'Sub | Dub',
+    src: 'Images/Posters/bluelock-poster.jpg',
+    link: 'https://www.crunchyroll.com/series/G4PH0WEKE/bluelock'
+  },
+
+  {
+    name: 'Bungo Stray Dogs',
+    info: 'Sub | Dub',
+    src: 'Images/Posters/bungo-stray-dogs-poster.jpg',
+    link: 'https://www.crunchyroll.com/series/GR5VXQ8PR/bungo-stray-dogs'
+  },
+
+  {
+    name: 'Fire Force',
+    info: 'Sub | Dub',
+    src: 'Images/Posters/fire-force-poster.jpg',
+    link: 'https://www.crunchyroll.com/series/GYQWNXPZY/fire-force'
+  },
+
+  {
+    name: 'VINLAND SAGA',
+    info: 'Sub | Dub',
+    src: 'Images/Posters/vinland-saga-poster.jpg',
+    link: 'https://www.crunchyroll.com/series/GEXH3WKK0/vinland-saga'
+  },
+
+  {
+    name: 'Dr. STONE',
+    info: 'Sub | Dub',
+    src: 'Images/Posters/dr-stone-poster.jpg',
+    link: 'https://www.crunchyroll.com/series/GYEXQKJG6/dr-stone'
+  },
+
+  {
+    name: 'Horimiya',
+    info: 'Sub | Dub',
+    src: 'Images/Posters/horimiya-poster.jpg',
+    link: 'https://www.crunchyroll.com/series/G9VHN9P43/horimiya'
+  },
+
+  {
+    name: 'Case Closed (Detective Conan',
+    info: 'Subtitled',
+    src: 'Images/Posters/case-closed-detective-conan-poster.jpg',
+    link: 'https://www.crunchyroll.com/series/G6JQVM3ER/case-closed-detective-conan'
+  },
+
+  {
+    name: 'Food Wars! Shokugeki no Soma',
+    info: 'Sub | Dub',
+    src: 'Images/Posters/food-wars-poster.jpg',
+    link: 'https://www.crunchyroll.com/series/G6GG91P26/food-wars-shokugeki-no-soma'
+  },
+
+  {
+    name: 'Fairy Tail',
+    info: 'Subtitled',
+    src: 'Images/Posters/fairy-tail-poster.jpg',
+    link: 'https://www.crunchyroll.com/series/G6DQDD3WR/fairy-tail'
+  },
+
+  {
+    name: 'Re:ZERO -Starting Life in Another World-',
+    info: 'Sub | Dub',
+    src: 'Images/Posters/re-zero-poster.jpg',
+    link: 'https://www.crunchyroll.com/series/GRGG9798R/rezero--starting-life-in-another-world-'
+  },
+
+  {
+    name: 'Golden Kamuy',
+    info: 'Sub | Dub',
+    src: 'Images/Posters/golden-kamuy-poster.jpg',
+    link: 'https://www.crunchyroll.com/series/GY8DWQN5Y/golden-kamuy'
+  },
+
+  {
+    name: 'Black Clover',
+    info: 'Sub | Dub',
+    src: 'Images/Posters/black-clover-poster.jpg',
+    link: 'https://www.crunchyroll.com/series/GRE50KV36/black-clover'
+  },
+
+  {
+    name: 'Blood Blockade Battlefront',
+    info: 'Sub | Dub',
+    src: 'Images/Posters/blood-blockade-poster.jpg',
+    link: 'https://www.crunchyroll.com/series/GRWEQEZER/blood-blockade-battlefront'
+  },
+
+  {
+    name: 'GOBLIN SLAYER',
+    info: 'Sub | Dub',
+    src: 'Images/Posters/goblin-slayer-poster.jpg',
+    link: 'https://www.crunchyroll.com/series/G6VDMN306/goblin-slayer'
+  },
+
+  {
+    name: 'Fate/stay night',
+    info: 'Sub | Dub',
+    src: 'Images/Posters/fate-stay-night-poster.jpg',
+    link: 'https://www.crunchyroll.com/series/GY8V11X7Y/fatestay-night'
+  },
+
+  {
+    name: 'Kill la Kill',
+    info: 'Subtitled',
+    src: 'Images/Posters/kill-la-kill-poster.jpg',
+    link: 'https://www.crunchyroll.com/series/GY8VM43GY/kill-la-kill'
+  },
+
+  {
+    name: 'Sword Art Online',
+    info: 'Sub | Dub',
+    src: 'Images/Posters/sword-art-online-poster.jpg',
+    link: 'https://www.crunchyroll.com/series/GR49G9VP6/sword-art-online'
+  },
+
+  {
+    name: 'Dragon Ball Z Kai',
+    info: 'Dubbed',
+    src: 'Images/Posters/dragon-ball-z-kai-poster.jpg',
+    link: 'https://www.crunchyroll.com/series/GMEHMEWP0/dragon-ball-z-kai'
+  },
+
+  {
+    name: 'DON\'T TOY WITH ME, MISS NAGATORO',
+    info: 'Sub | Dub',
+    src: 'Images/Posters/dont-toy-with-me-miss-nagatoro-poster.jpg',
+    link: 'https://www.crunchyroll.com/series/GQWH0M455/dont-toy-with-me-miss-nagatoro'
+  },
+
+  {
+    name: 'Chainsaw Man',
+    info: 'Sub | Dub',
+    src: 'Images/Posters/chainsaw-man-poster.jpg',
+    link: 'https://www.crunchyroll.com/series/GVDHX8QNW/chainsaw-man'
+  },
+
+  {
+    name: 'SPY X FAMILY',
+    info: 'Subtitlted',
+    src: 'Images/Posters/spy-x-family-poster.jpg',
+    link: 'https://www.crunchyroll.com/series/G4PH0WXVJ/spy-x-family'
+  },
+
+  {
+    name: 'Dragon Ball Z',
+    info: 'Sub | Dub',
+    src: 'Images/Posters/dragon-ball-z-poster.jpg',
+    link: 'https://www.crunchyroll.com/series/G9VHN9PPW/dragon-ball-z'
+  },
+
+  {
+    name: 'Dragon Ball',
+    info: 'Sub | Dub',
+    src: 'Images/Posters/dragon-ball-poster.jpg',
+    link: 'https://www.crunchyroll.com/series/G8DHV7W21/dragon-ball'
+  },
+
+  {
+    name: 'KAGUYA-SAMA: LOVE IS WAR',
+    info: 'Sub | Dub',
+    src: 'Images/Posters/kaguya-sama-poster.jpg',
+    link: 'https://www.crunchyroll.com/series/GRJ0J828Y/kaguya-sama-love-is-war'
+  },
+
+  {
+    name: 'Overlord',
+    info: 'Dubbed',
+    src: 'Images/Posters/overlord-poster.jpg',
+    link: 'https://www.crunchyroll.com/series/G69PZ5PDY/overlord'
+  },
+
+  {
+    name: 'To Your Eternity',
+    info: 'Sub | Dub',
+    src: 'Images/Posters/to-your-eternity-poster.jpg',
+    link: 'https://www.crunchyroll.com/series/GG5H5XMWV/to-your-eternity'
+  },
+
+  {
+    name: 'Psycho-Pass: Sinners of the System',
+    info: 'Sub | Dub',
+    src: 'Images/Posters/psycho-pass-sinners-of-the-system-poster.jpg',
+    link: 'https://www.crunchyroll.com/series/GVDHX8V05/psycho-pass-sinners-of-the-system'
+  },
+
+  {
+    name: 'My Dress-Up Darling',
+    info: 'Sub | Dub',
+    src: 'Images/Posters/my-dress-up-darling-poster.jpg',
+    link: 'https://www.crunchyroll.com/series/GQWH0M9N8/my-dress-up-darling'
+  },
+
+  {
+    name: 'Code Geass',
+    info: 'Sub | Dub',
+    src: 'Images/Posters/code-geass-poster.jpg',
+    link: 'https://www.crunchyroll.com/series/GY2P9ED0Y/code-geass'
+  },
+
+  {
+    name: 'Death Note',
+    info: 'Sub | Dub',
+    src: 'Images/Posters/death-note-poster.jpg',
+    link: 'https://www.crunchyroll.com/series/G6QWD3EE6/death-note'
+  },
+
+  {
+    name: 'Trigun',
+    info: 'Sub | Dub',
+    src: 'Images/Posters/trigun-poster.jpg',
+    link: 'https://www.crunchyroll.com/series/GYP58QMMY/trigun'
+  },
+
+  {
+    name: 'Attack on Titan',
+    info: 'Subtitled',
+    src: 'Images/Posters/attack-on-titan-poster.jpg',
+    link: 'https://www.crunchyroll.com/series/GR751KNZY/attack-on-titan'
+  },
+
+  {
+    name: 'Cowboy Bebop',
+    info: 'Sub | Dub',
+    src: 'Images/Posters/cowboy-bebop-poster.jpg',
+    link: 'https://www.crunchyroll.com/series/GYVNXMVP6/cowboy-bebop'
+  },
+
+  {
+    name: 'Ace Attorney',
+    info: 'Sub | Dub',
+    src: 'Images/Posters/ace-attorney-poster.jpg',
+    link: 'https://www.crunchyroll.com/series/GYE5K0XVR/ace-attorney'
+  },
+
+  {
+    name: 'BORUTO: NARUTO NEXT GENERATIONS',
+    info: 'Subtitled',
+    src: 'Images/Posters/boruto-poster.jpg',
+    link: 'https://www.crunchyroll.com/series/GR75Q020Y/boruto-naruto-next-generations'
+  },
+
+  {
+    name: 'Yu-Gi-Oh!',
+    info: 'Sub | Dub',
+    src: 'Images/Posters/yu-gi-oh-poster.jpg',
+    link: 'https://www.crunchyroll.com/series/G6P8DV7V6/yu-gi-oh'
+  },
+
+  {
+    name: 'JoJo\'s Bizarre Adventure',
+    info: 'Subtitled',
+    src: 'Images/Posters/jojos-bizarre-adventure-poster.jpg',
+    link: 'https://www.crunchyroll.com/series/GYP8DP1MY/jojos-bizarre-adventure'
+  },
+
+  {
+    name: 'Steins;Gate 0',
+    info: 'Sub | Dub',
+    src: 'Images/Posters/steins-gate-0-poster.jpg',
+    link: 'https://www.crunchyroll.com/series/GYW4D15K6/steinsgate-0'
+  },
+
+  {
+    name: 'Hunter x Hunter',
+    info: 'Subtitled',
+    src: 'Images/Posters/hunter-x-hunter-poster.jpg',
+    link: 'https://www.crunchyroll.com/series/GY3VKX1MR/hunter-x-hunter'
   }
 
 ]
+
+
+/*
+{
+  name: '',
+  info: '',
+  src: 'Images/Posters/',
+  link: ''
+}
+*/
